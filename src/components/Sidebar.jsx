ec61dcfac83afb9af127216b2a3154bb68cd38ec
Meysonsan <meyson443@gmail.com>
2024-02-13 22:16:58 +0200

    Add components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import { Stack, Button} from '@mui/material';
+
+import { categories } from '../utils/constant';
+
+
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
+    <Stack
+        direction="row"
+        sx={{
+            overflowY: "auto",
+            height: { sx: 'auto', md: '95%' },
+            flexDirection: { md: 'column'},
+        }}
+    >
+        {categories.map((category) => (
+            <Button 
+                variant="contained"
+                size="small"
+                startIcon={<span style={{ color: category.name === selectedCategory ? 'white' : 'red', display: 'flex' }}>{category.icon}</span>}
+                className="category-btn"
+                onClick={() => setSelectedCategory(category.name)}
+                style={{
+                    backgroundColor: category.name === selectedCategory ? '#FC1503' : '#000',
+                }}
+                key={category.name}
+            >
+                {category.name}
+            </Button>
+        ))}
+    </Stack>
+)
+
+export default Sidebar
---
+++ src/utils/constant.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon size="small" fontSize="small" />, },
+  { name: 'JS Mastery', icon: <CodeIcon size="small" fontSize="small" />, },
+  { name: 'Coding', icon: <CodeIcon size="small" fontSize="small" />, },
+  { name: 'ReactJS', icon: <CodeIcon size="small" fontSize="small" />, },
+  { name: 'NextJS', icon: <CodeIcon size="small" fontSize="small" />, },
+  { name: 'Music', icon: <MusicNoteIcon size="small" fontSize="small" /> },
+  { name: 'Education', icon: <SchoolIcon size="small" fontSize="small" />, },
+  { name: 'Podcast', icon: <GraphicEqIcon size="small" fontSize="small" />, },
+  { name: 'Movie', icon: <OndemandVideoIcon size="small" fontSize="small" />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon size="small" fontSize="small" />, },
+  { name: 'Live', icon: <LiveTvIcon size="small" fontSize="small" />, },
+  { name: 'Sport', icon: <FitnessCenterIcon size="small" fontSize="small" />, },
+  { name: 'Fashion', icon: <CheckroomIcon size="small" fontSize="small" />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon size="small" fontSize="small" />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon size="small" fontSize="small" />, },
+  { name: 'Gym', icon: <FitnessCenterIcon size="small" fontSize="small" />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon size="small" fontSize="small" />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
